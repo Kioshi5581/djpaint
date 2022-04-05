1bef880ef507cdb18d732d5fb04e59dc6ddc3399
Lachi <technicalsmirchis@gmail.com>
2022-04-05 22:03:48 +0500

    first commit

--- paint/views.py
+++ paint/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Files
 from django.views.decorators.csrf import csrf_exempt
-
 import json
 
 
 @csrf_exempt
 def paint(request):
     if request.method == 'GET':
-        return render(request, 'paint.html')
+        return render(request, 'paint/paint.html')
     elif request.method == 'POST':
         filename = request.POST['save_fname']
         data = request.POST['save_cdata']
@@ -22,11 +21,11 @@ def paint(request):
 def files(request):
     if request.method == 'GET':
         all_data = Files.objects.all()
-        return render(request, 'files.html', { 'files': all_data })
+        return render(request, 'paint/files.html', { 'files': all_data })
 
 def search(request):
     if 'filename' in request.GET:
         filename = request.GET['filename']
         datafile = Files.objects.get(name=filename)
-        return render(request, 'search.html', { 'data': datafile.canvas_image, 'filename': filename })
+        return render(request, 'paint/search.html', { 'data': datafile.canvas_image, 'filename': filename })
 
